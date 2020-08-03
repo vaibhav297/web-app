97bcb88307e630cb110f304aa224e7e54b81b546
vaibhav297 <31847547+vaibhav297@users.noreply.github.com>
2020-08-03 20:35:24 +0530

    Update code.py

--- app/code.py
+++ app/code.py
@@ -3,16 +3,13 @@ import openpyxl
 import logging
 def startx():
     sheet_name = ""
-    list= []
+    list =[]
     try:
-        workbook_master_data_tool = xlrd.open_workbook(r'Master_data.xlsm')
+        workbook_master_data_tool = xlrd.open_workbook(r'../Master_data.xlsm')
     except Exception as e:
-
         logging.error("please check file names and directories:")
         logging.error("../Master_data.xlsx")
-
     try:
-        
         for master_data in workbook_master_data_tool.sheets():
             if (master_data.name.lower()).startswith("MasterData".lower()):
                 master_data_sheet_name = master_data.name
@@ -20,13 +17,12 @@ def startx():
         for row_num1 in range(master_data_sheet.nrows):
             row_value1 = master_data_sheet.row_values(row_num1)
             print(row_value1)
-            list.append()
+            list.append(row_value1)
 
     except Exception as e:
         logging.error("please do not keep timesheet open")
         logging.error("please check file names and directories:")
         logging.error("C:\Finance Vision\Master_data.xlsx")
-     
     return list;
-startx()
+temp = startx()
  
