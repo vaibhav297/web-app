97359146246f5f6f80eebd73df7a424fd4ae8969
vaibhav297 <31847547+vaibhav297@users.noreply.github.com>
2020-08-03 18:18:19 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
-
+from app.code import startx
 from flask import Flask 
   
 app = Flask(__name__) 
   
 @app.route("/") 
-def home_view(): 
-        return "<h1>This is finance vision page</h1>"
+def home_view():
+        return startx()
