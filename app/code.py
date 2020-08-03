df25931efdea3c3f6f59bc1e24cddb8357e9e0a2
vaibhav297 <31847547+vaibhav297@users.noreply.github.com>
2020-08-03 20:00:26 +0530

    Update code.py

--- app/code.py
+++ app/code.py
@@ -3,6 +3,7 @@ import openpyxl
 import logging
 def startx():
     sheet_name = ""
+    list= []
     try:
         workbook_master_data_tool = xlrd.open_workbook(r'Master_data.xlsm')
     except Exception as e:
@@ -11,6 +12,7 @@ def startx():
         logging.error("../Master_data.xlsx")
 
     try:
+        
         for master_data in workbook_master_data_tool.sheets():
             if (master_data.name.lower()).startswith("MasterData".lower()):
                 master_data_sheet_name = master_data.name
@@ -18,6 +20,7 @@ def startx():
         for row_num1 in range(master_data_sheet.nrows):
             row_value1 = master_data_sheet.row_values(row_num1)
             print(row_value1)
+            list.append()
 
     except Exception as e:
         logging.error("please do not keep timesheet open")
@@ -25,5 +28,6 @@ def startx():
         logging.error("C:\Finance Vision\Master_data.xlsx")
 
        
-    return ;
-temp = startx()
+    return list;
+ temp=startx()
+ 
