91e73d4c69c60039f2598548c82cd98243e99a6f
vaibhav297 <vsahastrabuddhe060@gmail.com>
2020-08-04 14:01:05 +0530

    templates, static directory added

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
-from app.code import startx,temp
+from app.code import startx,temp, render_template
 from flask import Flask
   
 app = Flask(__name__) 
   
 @app.route("/") 
 def home_view():
-        return "<p>"+str(temp)+"</p>"
+        return render_template("home.html")
+@app.route("/DashBoard")
+def dashboard():
+    return "<p>"+str(temp)+"</p>"
